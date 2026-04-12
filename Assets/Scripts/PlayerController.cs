361a31926aef2a165f8bf809a3ace25589120f67
Russ Perry <rperry1886@gmail.com>
2026-04-12 18:32:08 -0400

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Assign a new Rigidbody variable
+    private Rigidbody rb;
+
+    // Movement along the X and Y axes.
+    private float movementX;
+    private float movementY;
+
+    // Speed at which the player moves.
+    public float speed = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        // Set the movement vector
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+}
