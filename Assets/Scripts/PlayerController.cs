3aa7d2a43e5cdc524a910d927aa9976d4eef08b4
Russ Perry <rperry1886@gmail.com>
2026-04-12 20:36:17 -0400

    Add Counter and Win Con
    There is now a counter on screen to see how many collectables you picked up. Additionally, when all collectables are collected, you see a 'You Win' screen.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro; // For TextMeshPro
 
 public class PlayerController : MonoBehaviour
 {
     // Assign a new Rigidbody variable
     private Rigidbody rb;
+
+    // Create a coutner for points
+    private int count;
+
+    // Variable for the win text
+    public GameObject winTextObject;
+
 
     // Movement along the X and Y axes.
     private float movementX;
@@ -13,10 +21,16 @@ public class PlayerController : MonoBehaviour
     // Speed at which the player moves.
     public float speed = 0;
 
+    // Declare the text variable
+    public TextMeshProUGUI countText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -25,6 +39,15 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Collected: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
@@ -38,6 +61,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
         }
     }
 
